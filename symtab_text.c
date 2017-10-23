a6b86e3adf6bab41cbb2d09e120cdf2352d5e379
743834110 <743834110@qq.com>
2017-10-23 10:23:33 +0800

    测试提交2

--- symtab_text.c
+++ symtab_text.c
@@ -6,21 +6,21 @@ char * words[12] = {
    "graph", "memory", "path", "tree", "unary", "video"
 };
 
-//int main(){
-//	int i;
-//	printf("[insert]\n");
-//	for (i = 0; i < 12; i ++){
-//		printf("%s  ", words[i]);
-//		sym_insert(words[i], K_VAR,0);
-//	}
-//	printf("\n\n[find]\n");
-//	
-//	char * a[4] = { "sort", "unary", "binary", "tirnary" };
-//	for (i = 0; i < 4; i ++){
-//		if (sym_find(a[i]) != NULL)
-//			printf("%-8s:found.\n", a[i]);
-//		else
-//			printf("%-8s:not found.\n",a[i]);
-//	}
-//	return 0;
-//}
+int main(){
+	int i;
+	printf("[insert]\n");
+	for (i = 0; i < 12; i ++){
+		printf("%s  ", words[i]);
+		sym_insert(words[i], K_VAR,0);
+	}
+	printf("\n\n[find]\n");
+	
+	char * a[4] = { "sort", "unary", "binary", "tirnary" };
+	for (i = 0; i < 4; i ++){
+		if (sym_find(a[i]) != NULL)
+			printf("%-8s:found.\n", a[i]);
+		else
+			printf("%-8s:not found.\n",a[i]);
+	}
+	return 0;
+}
